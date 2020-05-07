372a368aec2526f2bf43b905cbcb3c888fe935ac
Leonardo Souza Dias <leonnardosouza96@gmail.com>
2020-05-07 18:50:03 -0300

    alteracao na versao do node para conexao com o mongodb

--- server.js
+++ server.js
@@ -7,15 +7,18 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb+srv://nodeapi:node@nodeapi-pfmrw.mongodb.net/test?retryWrites=true&w=majority',
+//mongoose.connect('mongodb+srv://leonardo:leo123456@nodeapi-pfmrw.mongodb.net/test?retryWrites=true&w=majority',
+mongoose.connect('mongodb://leonardo:leo123456@nodeapi-shard-00-00-pfmrw.mongodb.net:27017,nodeapi-shard-00-01-pfmrw.mongodb.net:27017,nodeapi-shard-00-02-pfmrw.mongodb.net:27017/test?ssl=true&replicaSet=nodeapi-shard-0&authSource=admin&retryWrites=true&w=majority',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
-    .then(() => console.log('BD Conectado'))
+    .then(() => console.log('Banco de Dados Conectado'))
     .catch(err => {
-        console.log(`Erro de conexão do BD: ${err.message}`);
+        console.log(`Erro de conexão do Banco de Dados: ${err.message}`);
     });
+
+
 
 requireDir("./src/models");
 
